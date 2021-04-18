c56f3e116c5290e07158115b3d511e3957f28a06
Vlad Popko <vladpopko@gmail.com>
2021-04-18 21:39:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let methodsForList = {
+  addNewTask : function() {
+        console.log('asdas');
+      },
+  deleteTask : function() {
+        console.log('asdas');
+      },
+  editTask : function() {
+        console.log('asdas');
+      },
+  viewList : function() {
+        console.log('asdas');
+      },
+
+}
+
+methodsForList.addNewTask();
