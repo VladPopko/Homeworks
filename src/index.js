8a89e5567449e44eeeec0cd4728af836a1c756bf
Vlad Popko <vladpopko@gmail.com>
2021-05-30 20:45:53 +0300

    fixed first task

--- src/index.js
+++ src/index.js
@@ -1,56 +1,53 @@
 'use strict';
 // First task
 
-
 // const $wrapper = document.querySelector('.wrapper');
-// let counter = 0;
 // const zero = 0;
-// const someNumber = 1;
-// function generateList(array, $place,startValue){
+
+// function generateList(array, $place){
 
 //     $place.append(document.createElement('ul'));
-//     const $ulList = document.querySelectorAll('ul');
-//     counter++;
-//     for (let i = counter-someNumber; i < $ulList.length; i++){
-//         for (let j = 0; j < array.length; j++){
-//             if(!(Array.isArray(array[j]))){
-//                 $ulList[i].innerHTML += `<li>${array[j]}</li>`;
-//             } else{
-//                 $ulList[i].innerHTML += '<li></li>';
-//             }
-//             const $li = document.querySelectorAll('li');
-//             if (Array.isArray(array[j])){
-//                 generateList(array[j] , $li[startValue],startValue+someNumber);
-//             }
-//             startValue++;
+//     const $ul = $place.children[zero];
+//     for (let i = 0; i < array.length; i++){
+//         $ul.append(document.createElement('li'));
+//         if (Array.isArray(array[i])){
+//             generateList(array[i] , $ul.children[i]);
+//         } else{
+//             $ul.children[i].innerText = array[i];
 //         }
 //     }
+
 // }
 
-// // const magicNumbers = [1,2, [1.1,1.2,[1.1,[3.2]],1.3] ,3];
+
+// const magicNumbers = [1,2, [1.1,1.2,1.3,[1.1,1.2,1.3]] ,3];
 
 
-// // generateList(magicNumbers , $wrapper, zero);
+// generateList(magicNumbers , $wrapper);
+
 
 // Second task
 
-const $wrapper = document.querySelector('.wrapper');
+// const $wrapper = document.querySelector('.wrapper');
 
-const number = 10;
-let counter = 1;
-$wrapper.innerHTML = '<table><tbody></tbody></table>';
-const $tbody = document.querySelector('tbody');
-const $table = document.querySelector('table');
+// const number = 10;
+// let counter = 1;
+// $wrapper.innerHTML = '<table><tbody></tbody></table>';
+// const $tbody = document.querySelector('tbody');
+// const $table = document.querySelector('table');
 
-$table.style.textAlign = 'center';
+// $table.style.textAlign = 'center';
 
-for (let i = 1; i <= number; i++){
-    $tbody.innerHTML += '<tr></tr>';
-}
-const $tr = document.querySelectorAll('tr');
+// for (let i = 1; i <= number; i++){
+//     $tbody.innerHTML += '<tr></tr>';
+// }
+// const $tr = document.querySelectorAll('tr');
 
-for (let i = 0; i < number; i++){
-    for (let j = 0;j < number; j++){
-        $tr[i].innerHTML += `<td style="border: 1px solid black;">${counter++}</td>`;
-    }
-}
+// for (let i = 0; i < number; i++){
+//     for (let j = 0;j < number; j++){
+//         $tr[i].innerHTML += `<td style="border: 1px solid black;">${counter++}</td>`;
+//     }
+// }
+
+
+
